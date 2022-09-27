9021d3f2a6da068af11183b692b1a327a6de6428
pavtlly <pavtlly@gmail.com>
2022-09-27 09:06:36 +0800

    Delete test.lua

--- lua/test.lua
+++
@@ -1,8 +0,0 @@
--- __index是一个表
-local mp1 = {name = "wc", age = 23}
-local mt2 = {
-    __index = mp1 -- index对应的表
-}
-local b = {sex = "boy"}
-setmetatable(b, mt2)
-print(b.sex, b.name, b.age, b.enjoy)
